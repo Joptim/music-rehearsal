f01670f2c4b54266a481dac4b6b81d3ed4cd60b6
joptim <28574850+Joptim@users.noreply.github.com>
2021-05-09 22:02:28 +0200

    Implement note.go

--- theory/note/note.go
+++ theory/note/note.go
@@ -2,37 +2,133 @@ package note
 
 import (
 	"fmt"
+	"github.com/Joptim/music-rehearsal/theory/natural"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 type Note struct {
-	Natural string
-	SharpAccidentals int
-	FlatAccidentals int
+	natural natural.Natural
+	octave  int
+	// accidental equals -1 for flat notes,
+	// 0 for natural notes and 1 for sharp notes.
+	accidental int
 }
 
-func New(name string) (Note, error){
+func (n Note) GetName() string {
+	var accidental string
+	switch n.accidental {
+	case -1:
+		accidental = "b"
+	case 0:
+		accidental = ""
+	case 1:
+		accidental = "#"
+	}
+	return fmt.Sprintf("%s%s%d", n.natural.GetName(), accidental, n.octave)
+}
+
+func (n Note) SemitonesFrom(from Note) int {
+	return n.semitones() - from.semitones()
+}
+
+// semitones return the number of semitones from A0
+func (n Note) semitones() int {
+	semitones := n.natural.Semitones()
+	return 12*n.octave + semitones + n.accidental
+}
+
+func (n Note) AddSemitone() Note {
+	note := n
+	switch n.accidental {
+	case 0:
+		if note.natural.SemitonesToNext() == 2 {
+			note.accidental = 1
+			break
+		}
+		fallthrough
+	case 1:
+		note.natural, _ = n.natural.Next()
+		if note.natural.IsA() {
+			note.octave += 1
+		}
+		fallthrough
+	case -1:
+		note.accidental = 0
+	}
+	return note
+}
+
+func (n Note) SubtractSemitone() Note {
+	note := n
+	switch n.accidental {
+	case 0:
+		if note.natural.SemitonesFromPrev() == 2 {
+			note.accidental = -1
+			break
+		}
+		fallthrough
+	case -1:
+		note.natural, _ = n.natural.Prev()
+		if n.natural.IsA() {
+			note.octave -= 1
+		}
+		fallthrough
+	case 1:
+		note.accidental = 0
+	}
+	return note
+}
+
+func (n Note) AddSemitones(semitones int) Note {
+	note := n
+	if semitones >= 0 {
+		for st := 1; st <= semitones; st++ {
+			note = note.AddSemitone()
+		}
+	} else {
+		for st := -1; st >= semitones; st-- {
+			note = note.SubtractSemitone()
+		}
+	}
+	return note
+}
+
+func New(name string) (Note, error) {
 	re := regexp.MustCompile(
-	"^(?P<natural>[A-G]{1})" +
-		"(?P<accidental>bb|b|##|#)?" +
-		"(?P<octave>[0-7]{1})$")
+		"^(?P<natural>[A-G]{1})" +
+			"(?P<accidental>b|#)?" +
+			"(?P<octave>[0-7]{1})$")
 	match := re.FindStringSubmatch(name)
 	if len(match) == 0 {
 		return Note{}, fmt.Errorf("cannot create note with name %s", name)
 	}
 
-	accidentals := match[2]
-	sharpAccidentals, flatAccidentals := 0, 0
-	if strings.Contains(accidentals, "#") {
-		sharpAccidentals = len(accidentals)
+	// Natural
+	nat, err := natural.NewNatural(match[1])
+	if err != nil {
+		return Note{}, err
 	}
-	if strings.Contains(accidentals, "b") {
-		flatAccidentals = len(accidentals)
+
+	// Accidentals
+	accidental := 0
+	if strings.Contains(match[2], "#") {
+		accidental = 1
 	}
-	return Note {
-		Natural: fmt.Sprintf("%s%s", match[1], match[3]),
-		SharpAccidentals: sharpAccidentals,
-		FlatAccidentals: flatAccidentals,
+	if strings.Contains(match[2], "b") {
+		accidental = -1
+	}
+
+	// Octave
+	octave, err := strconv.Atoi(match[3])
+	if err != nil {
+		return Note{}, err
+	}
+
+	return Note{
+		natural:    nat,
+		octave:     octave,
+		accidental: accidental,
 	}, nil
 }
--- theory/note/note_test.go
+++ theory/note/note_test.go
@@ -1,38 +1,316 @@
 package note
 
-import "testing"
+import (
+	"github.com/Joptim/music-rehearsal/theory/natural"
+	"testing"
+)
 
-func TestNew(t *testing.T){
-	var table = []struct {
-		in string
+func TestNew(t *testing.T) {
+	table := []struct {
+		in       string
 		expected Note
 	}{
-		{"A4", Note{"A4", 0, 0}},
-		{"Ab4", Note{"A4", 0, 1}},
-		{"Abb4", Note{"A4", 0, 2}},
-		{"A#4", Note{"A4", 1, 0}},
-		{"A##4", Note{"A4", 2, 0}},
-		{"E#4", Note{"E4", 1, 0}},
-		{"Fb4", Note{"F4", 0, 1}},
+		{"A0", Note{natural.NewNaturalTestHelper("A", t), 0, 0}},
+		{"Bb1", Note{natural.NewNaturalTestHelper("B", t), 1, -1}},
+		{"B1", Note{natural.NewNaturalTestHelper("B", t), 1, 0}},
+		{"B#1", Note{natural.NewNaturalTestHelper("B", t), 1, 1}},
+		{"Cb2", Note{natural.NewNaturalTestHelper("C", t), 2, -1}},
+		{"C2", Note{natural.NewNaturalTestHelper("C", t), 2, 0}},
+		{"C#2", Note{natural.NewNaturalTestHelper("C", t), 2, 1}},
+		{"Db3", Note{natural.NewNaturalTestHelper("D", t), 3, -1}},
+		{"D3", Note{natural.NewNaturalTestHelper("D", t), 3, 0}},
+		{"D#3", Note{natural.NewNaturalTestHelper("D", t), 3, 1}},
+		{"Eb4", Note{natural.NewNaturalTestHelper("E", t), 4, -1}},
+		{"E4", Note{natural.NewNaturalTestHelper("E", t), 4, 0}},
+		{"E#4", Note{natural.NewNaturalTestHelper("E", t), 4, 1}},
+		{"Fb5", Note{natural.NewNaturalTestHelper("F", t), 5, -1}},
+		{"F5", Note{natural.NewNaturalTestHelper("F", t), 5, 0}},
+		{"F#5", Note{natural.NewNaturalTestHelper("F", t), 5, 1}},
+		{"Gb6", Note{natural.NewNaturalTestHelper("G", t), 6, -1}},
+		{"G6", Note{natural.NewNaturalTestHelper("G", t), 6, 0}},
+		{"G#6", Note{natural.NewNaturalTestHelper("G", t), 6, 1}},
+		{"Ab7", Note{natural.NewNaturalTestHelper("A", t), 7, -1}},
 	}
 	for _, test := range table {
 		actual, err := New(test.in)
 		if err != nil {
-			t.Logf("got error %s, expected %v", err, test.expected)
+			t.Logf("with %s, got error %s, expected %v", test.in, err, test.expected)
 			t.FailNow()
 		}
 		if actual != test.expected {
-			t.Errorf("got %v, expected %v", actual, test.expected)
+			t.Errorf("with %s, got %v, expected %v", test.in, actual, test.expected)
 		}
 	}
 }
 
-func TestNewFailsOnInvalidName(t *testing.T){
-	var table = []string {"A", "A-1", "A3b", "A#b3", "Ab#3", "Abbb3", "AB3", "A###3", "H3"}
+func TestNewFailsOnInvalidName(t *testing.T) {
+	table := []string{
+		"A",
+		"A-1",
+		"A3b",
+		"A#b3",
+		"Abb3",
+		"Ab#3",
+		"Abbb3",
+		"AB3",
+		"A##3",
+		"A###3",
+		"H3",
+	}
 	for _, invalidNote := range table {
 		_, err := New(invalidNote)
 		if err == nil {
-			t.Errorf("got nil error for invalid note %s, expected non-nil error", invalidNote)
+			t.Errorf("with %s, got nil error, expected non-nil error", invalidNote)
 		}
 	}
+}
+
+func TestNote_GetName(t *testing.T) {
+	table := []string{
+		"A0",
+		"Bb1",
+		"B1",
+		"B#1",
+		"Cb2",
+		"C2",
+		"C#2",
+		"Db3",
+		"D3",
+		"D#3",
+		"Eb4",
+		"E4",
+		"E#4",
+		"Fb5",
+		"F5",
+		"F#5",
+		"Gb6",
+		"G6",
+		"G#6",
+		"Ab7",
+	}
+	for _, name := range table {
+		note := NewHelper(name, t)
+		actual := note.GetName()
+		if actual != name {
+			t.Errorf("with %s, got %s, expected %s", name, actual, name)
+		}
+	}
+}
+
+func TestNote_Semitones(t *testing.T) {
+	table := []struct {
+		name     string
+		expected int
+	}{
+		{"A0", 0},
+		{"Bb1", 13},
+		{"B1", 14},
+		{"B#1", 15},
+		{"Cb2", 26},
+		{"C2", 27},
+		{"C#2", 28},
+		{"Db3", 40},
+		{"D3", 41},
+		{"D#3", 42},
+		{"Eb4", 54},
+		{"E4", 55},
+		{"E#4", 56},
+		{"Fb5", 67},
+		{"F5", 68},
+		{"F#5", 69},
+		{"Gb6", 81},
+		{"G6", 82},
+		{"G#6", 83},
+		{"Ab7", 83},
+	}
+	for _, test := range table {
+		note := NewHelper(test.name, t)
+		actual := note.semitones()
+		if actual != test.expected {
+			t.Errorf("with %s, got %v, expected %v", test.name, actual, test.expected)
+		}
+	}
+}
+
+func TestNote_SemitonesFrom(t *testing.T) {
+	table := []struct {
+		from     string
+		note     string
+		expected int
+	}{
+		{"C3", "C3", 0},
+		{"C3", "C#3", 1},
+		{"C3", "Db3", 1},
+		{"C3", "D3", 2},
+		{"C3", "D#3", 3},
+		{"C3", "Eb3", 3},
+		{"C3", "E3", 4},
+		{"C3", "F3", 5},
+		{"C3", "F#3", 6},
+		{"C3", "Gb3", 6},
+		{"C3", "G3", 7},
+		{"C3", "G#3", 8},
+		{"C3", "Ab4", 8},
+		{"C3", "A4", 9},
+		{"C3", "A#4", 10},
+		{"C3", "Bb4", 10},
+		{"C3", "B4", 11},
+		{"C3", "C4", 12},
+		{"C3", "B3", -1},
+		{"C3", "Bb3", -2},
+		{"C3", "A#3", -2},
+		{"C3", "A3", -3},
+		{"C3", "Ab3", -4},
+		{"C3", "G#2", -4},
+		{"C3", "G2", -5},
+		{"C3", "Gb2", -6},
+		{"C3", "F#2", -6},
+		{"C3", "F2", -7},
+		{"C3", "E2", -8},
+		{"C3", "Eb2", -9},
+		{"C3", "D#2", -9},
+		{"C3", "D2", -10},
+		{"C3", "Db2", -11},
+		{"C3", "C#2", -11},
+		{"C3", "C2", -12},
+	}
+
+	for _, test := range table {
+		note := NewHelper(test.note, t)
+		fromNote := NewHelper(test.from, t)
+		actual := note.SemitonesFrom(fromNote)
+		if actual != test.expected {
+			t.Errorf("with %s and %s, got %v, expected %v", test.from, test.note, actual, test.expected)
+		}
+	}
+}
+
+func TestNote_AddSemitone(t *testing.T) {
+	table := []struct {
+		note     Note
+		expected Note
+	}{
+		{NewHelper("C3", t), NewHelper("C#3", t)},
+		{NewHelper("C#3", t), NewHelper("D3", t)},
+		{NewHelper("D3", t), NewHelper("D#3", t)},
+		{NewHelper("D#3", t), NewHelper("E3", t)},
+		{NewHelper("E3", t), NewHelper("F3", t)},
+		{NewHelper("F3", t), NewHelper("F#3", t)},
+		{NewHelper("F#3", t), NewHelper("G3", t)},
+		{NewHelper("G3", t), NewHelper("G#3", t)},
+		{NewHelper("G#3", t), NewHelper("A4", t)},
+		{NewHelper("A4", t), NewHelper("A#4", t)},
+		{NewHelper("A#4", t), NewHelper("B4", t)},
+		{NewHelper("Ab3", t), NewHelper("A3", t)},
+		{NewHelper("Bb3", t), NewHelper("B3", t)},
+		{NewHelper("Cb3", t), NewHelper("C3", t)},
+		{NewHelper("Db3", t), NewHelper("D3", t)},
+		{NewHelper("Eb3", t), NewHelper("E3", t)},
+		{NewHelper("Fb3", t), NewHelper("F3", t)},
+		{NewHelper("Gb3", t), NewHelper("G3", t)},
+	}
+	for _, test := range table {
+		actual := test.note.AddSemitone()
+		if test.expected != actual {
+			t.Errorf(
+				"from %v, got %v, expected %v",
+				test.note,
+				actual,
+				test.expected,
+			)
+		}
+	}
+}
+
+func TestNote_SubtractSemitone(t *testing.T) {
+	table := []struct {
+		note     Note
+		expected Note
+	}{
+		{NewHelper("C3", t), NewHelper("B3", t)},
+		{NewHelper("B3", t), NewHelper("Bb3", t)},
+		{NewHelper("Bb3", t), NewHelper("A3", t)},
+		{NewHelper("A3", t), NewHelper("Ab3", t)},
+		{NewHelper("Ab3", t), NewHelper("G2", t)},
+		{NewHelper("G2", t), NewHelper("Gb2", t)},
+		{NewHelper("Gb2", t), NewHelper("F2", t)},
+		{NewHelper("F2", t), NewHelper("E2", t)},
+		{NewHelper("E2", t), NewHelper("Eb2", t)},
+		{NewHelper("Eb2", t), NewHelper("D2", t)},
+		{NewHelper("D2", t), NewHelper("Db2", t)},
+		{NewHelper("Db2", t), NewHelper("C2", t)},
+		{NewHelper("A#3", t), NewHelper("A3", t)},
+		{NewHelper("B#3", t), NewHelper("B3", t)},
+		{NewHelper("C#3", t), NewHelper("C3", t)},
+		{NewHelper("D#3", t), NewHelper("D3", t)},
+		{NewHelper("E#3", t), NewHelper("E3", t)},
+		{NewHelper("F#3", t), NewHelper("F3", t)},
+		{NewHelper("G#3", t), NewHelper("G3", t)},
+	}
+	for _, test := range table {
+		actual := test.note.SubtractSemitone()
+		if test.expected != actual {
+			t.Errorf(
+				"from %v, got %v, expected %v",
+				test.note,
+				actual,
+				test.expected,
+			)
+		}
+	}
+}
+
+func TestNote_AddSemitones(t *testing.T) {
+	table := []struct {
+		note      Note
+		semitones int
+		expected  Note
+	}{
+		{NewHelper("C3", t), 0, NewHelper("C3", t)},
+		{NewHelper("C3", t), 1, NewHelper("C#3", t)},
+		{NewHelper("C3", t), 2, NewHelper("D3", t)},
+		{NewHelper("C3", t), 3, NewHelper("D#3", t)},
+		{NewHelper("C3", t), 4, NewHelper("E3", t)},
+		{NewHelper("C3", t), 5, NewHelper("F3", t)},
+		{NewHelper("C3", t), 6, NewHelper("F#3", t)},
+		{NewHelper("C3", t), 7, NewHelper("G3", t)},
+		{NewHelper("C3", t), 8, NewHelper("G#3", t)},
+		{NewHelper("C3", t), 9, NewHelper("A4", t)},
+		{NewHelper("C3", t), 10, NewHelper("A#4", t)},
+		{NewHelper("C3", t), 11, NewHelper("B4", t)},
+		{NewHelper("C3", t), 12, NewHelper("C4", t)},
+		{NewHelper("C3", t), -1, NewHelper("B3", t)},
+		{NewHelper("C3", t), -2, NewHelper("Bb3", t)},
+		{NewHelper("C3", t), -3, NewHelper("A3", t)},
+		{NewHelper("C3", t), -4, NewHelper("Ab3", t)},
+		{NewHelper("C3", t), -5, NewHelper("G2", t)},
+		{NewHelper("C3", t), -6, NewHelper("Gb2", t)},
+		{NewHelper("C3", t), -7, NewHelper("F2", t)},
+		{NewHelper("C3", t), -8, NewHelper("E2", t)},
+		{NewHelper("C3", t), -9, NewHelper("Eb2", t)},
+		{NewHelper("C3", t), -10, NewHelper("D2", t)},
+		{NewHelper("C3", t), -11, NewHelper("Db2", t)},
+		{NewHelper("C3", t), -12, NewHelper("C2", t)},
+	}
+	for _, test := range table {
+		actual := test.note.AddSemitones(test.semitones)
+		if test.expected != actual {
+			t.Errorf(
+				"from %v and %d, got %v, expected %v",
+				test.note,
+				test.semitones,
+				actual,
+				test.expected,
+			)
+		}
+	}
+}
+
+func NewHelper(name string, t *testing.T) Note {
+	t.Helper()
+	note, err := New(name)
+	if err != nil {
+		t.Fatalf("cannot instantiate note with name %s. Got error %v", name, err)
+	}
+	return note
 }
